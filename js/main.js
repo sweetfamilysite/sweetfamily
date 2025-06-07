110a1f342b39dcb02c055fd17d4567b9b3cb338e
sweetfamilysite <lecrispim87@gmail.com>
2025-06-08 00:02:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -93,13 +93,3 @@ loadSection('home');
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('menu-toggle');
-  const menu = document.getElementById('menu');
-
-  if (toggle && menu) {
-    toggle.addEventListener('click', () => {
-      menu.classList.toggle('show');
-    });
-  }
-});
